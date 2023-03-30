a762366d442067f82732fa9b7806e495124b53fb
iraticasi <irati.casi@540deg.com>
2023-03-30 17:44:03 +0200

    refactor: merge conditions by type

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -29,6 +29,9 @@ const updateItemQuality = (item: Item) => {
     if (item.quality < 50) {
       item.quality = item.quality + 1
     }
+    if (item.quality < 50 && item.sellIn <= 0) {
+      item.quality = item.quality + 1
+    }
   }
 
   if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -42,27 +45,19 @@ const updateItemQuality = (item: Item) => {
       }
 
     }
+  if (item.sellIn <= 0) {
+    item.quality = item.quality - item.quality
+  }
   }
 
-  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros') {
+
+  if (!(item.name == 'Aged Brie') && item.name !== 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros') {
+    if (item.quality > 0 && item.sellIn <= 0) {
+      item.quality = item.quality - 1
+    }
     if (item.quality > 0) {
       item.quality = item.quality - 1
     }
-  }
-
-
-
-  if (item.name == 'Aged Brie') {
-    if (item.quality < 50 && item.sellIn <= 0) {
-      item.quality = item.quality + 1
-    }
-  }
-  if (!(item.name == 'Aged Brie') && item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sellIn <= 0) {
-    item.quality = item.quality - item.quality
-  }
-  if (!(item.name == 'Aged Brie') && item.name !== 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros' && item.sellIn <= 0) {
-    if (item.quality > 0)
-      item.quality = item.quality - 1
   }
 
   if (item.name != 'Sulfuras, Hand of Ragnaros') {
