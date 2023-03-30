53eada6bc756b1c631c834ff5156fc2cef3e0e6b
iraticasi <irati.casi@540deg.com>
2023-03-30 17:59:55 +0200

    refactor: extract update quality methods by type

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -25,41 +25,61 @@ export class GildedRose {
 
 
 const updateItemQuality = (item: Item) => {
-  if (item.name == 'Aged Brie') {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1
-    }
-    if (item.quality < 50 && item.sellIn <= 0) {
-      item.quality = item.quality + 1
-    }
-  } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1
-      if (item.sellIn <= 10) {
-        item.quality = item.quality + 1
-      }
-      if (item.sellIn <= 5) {
-        item.quality = item.quality + 1
-      }
-
-    }
-    if (item.sellIn <= 0) {
-      item.quality = item.quality - item.quality
-    }
-  } else if (item.name == 'Sulfuras, Hand of Ragnaros') {
-
-  } else {
-    if (item.quality > 0 && item.sellIn <= 0) {
-      item.quality = item.quality - 1
-    }
-    if (item.quality > 0) {
-      item.quality = item.quality - 1
-    }
+  switch (item.name) {
+    case 'Aged Brie':
+      updateAgedBrieQuality(item)
+      break;
+    case 'Backstage passes to a TAFKAL80ETC concert':
+      updateBackStageQuality(item)
+      break;
+    case 'Sulfuras, Hand of Ragnaros':
+      updateSulfurasQuality(item)
+      break;
+    default:
+      updateBasicItem(item)
+      break;
   }
-
 
   if (item.name != 'Sulfuras, Hand of Ragnaros') {
     item.sellIn = item.sellIn - 1;
   }
 
+}
+
+const updateAgedBrieQuality = (item: Item) => {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1
+  }
+  if (item.quality < 50 && item.sellIn <= 0) {
+    item.quality = item.quality + 1
+  }
+}
+
+const updateBackStageQuality = (item: Item) => {
+  if (item.quality < 50) {
+    item.quality = item.quality + 1
+    if (item.sellIn <= 10) {
+      item.quality = item.quality + 1
+    }
+    if (item.sellIn <= 5) {
+      item.quality = item.quality + 1
+    }
+
+  }
+  if (item.sellIn <= 0) {
+    item.quality = item.quality - item.quality
+  }
+};
+
+const updateSulfurasQuality = (item: Item) => {
+  //do nothing
+}
+
+const updateBasicItem = (item: Item) => {
+  if (item.quality > 0 && item.sellIn <= 0) {
+    item.quality = item.quality - 1
+  }
+  if (item.quality > 0) {
+    item.quality = item.quality - 1
+  }
 }
